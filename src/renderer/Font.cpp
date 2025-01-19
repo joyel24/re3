02a83d32aa34e851472a48c1f026049b6017d947
schm1dtmac <danonmac@icloud.com>
2025-01-19 20:21:03 +0000

    Update Font.cpp

--- src/renderer/Font.cpp
+++ src/renderer/Font.cpp
@@ -1463,7 +1463,7 @@ CFont::SetScale(float x, float y)
 		y *= 1.25f;
 	}*/
 #endif
-	Details.scaleX = y * 0.5f;
+	Details.scaleX = (x > y / 1.6875f) ? (y / 1.6875f) : x; 
 	Details.scaleY = y;
 }
 
