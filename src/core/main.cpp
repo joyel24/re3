6169979cfff5b49d21296fdbf6e811247847df5d
schm1dtmac <danonmac@icloud.com>
2025-01-19 22:35:35 +0000

    Update main.cpp

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -2441,14 +2441,16 @@ void PlayIntroMPEGs()
 int
 main(int argc, char *argv[])
 {
-	char path[1024];
-	uint32_t size = sizeof(path);
-	_NSGetExecutablePath(path, &size);
-	unsigned found = std::string(path).find_last_of("/");
-	std::string final = std::string(path).substr(0,found);
-	found = final.find_last_of("/");
-	final = std::string(path).substr(0,found) + std::string("/Resources/");
-	chdir(final.c_str());
+	char path[PATH_MAX];
+	uint32_t pathLen = sizeof(path);
+	int err = _NSGetExecutablePath(path, &pathLen);
+	assert(!err);
+	chdir(dirname(path));
+	chdir("../Resources");
+
+	char cwd[PATH_MAX];
+	getcwd(cwd, sizeof(cwd));
+	printf("Working directory is %s\n", cwd);
 #ifdef __MWERKS__
 	mwInit(); // metrowerks initialisation
 #endif
