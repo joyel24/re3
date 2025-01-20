21b9b54f3c5fc1953f5dcb14f1fac0718c2adfa4
schm1dtmac <danonmac@icloud.com>
2025-01-20 21:40:40 +0000

    Update glfw.cpp

--- src/skel/glfw/glfw.cpp
+++ src/skel/glfw/glfw.cpp
@@ -2219,7 +2219,7 @@ main(int argc, char *argv[])
 #ifdef PS2_MENU
 						gGameState = GS_INIT_PLAYING_GAME;
 #else
-						gGameState = GS_INIT_PLAYING_GAME;
+						gGameState = GS_INIT_FRONTEND;
 						TRACE("gGameState = GS_INIT_FRONTEND;");
 #endif
 						break;
