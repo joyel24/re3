8bee18321b62c9012552e003f833b61d3127fa8d
schm1dtmac <danonmac@icloud.com>
2025-01-23 17:24:41 +0000

    Update PCSave.cpp

--- src/save/PCSave.cpp
+++ src/save/PCSave.cpp
@@ -25,15 +25,9 @@ C_PcSave::SetSaveDirectory(const char *path)
 bool
 C_PcSave::DeleteSlot(int32 slot)
 {
-#ifdef FIX_BUGS
-	char FileName[MAX_PATH];
-#else
-	char FileName[200];
-#endif
-
+	MakeValidSaveName(slot);
 	PcSaveHelper.nErrorCode = SAVESTATUS_SUCCESSFUL;
-	sprintf(FileName, "%s%i.b", DefaultPCSaveFileName, slot + 1);
-	DeleteFile(FileName);
+	DeleteFile(ValidSaveName);
 	SlotSaveDate[slot][0] = '\0';
 	return true;
 }
