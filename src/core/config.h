9f29f7e751cf6d50f6d80fb38e4f66b4b751d70c
schm1dtmac <danonmac@icloud.com>
2025-01-23 16:21:46 +0000

    Update config.h

--- src/core/config.h
+++ src/core/config.h
@@ -339,7 +339,7 @@ enum Config {
 #define FIX_RADAR			// use radar size from early version before R* broke it
 #define XBOX_SUBTITLES	// the infamous outlines
 #define RADIO_OFF_TEXT
-#define PC_MENU
+//#define PC_MENU
 
 #ifndef PC_MENU
 #	define PS2_MENU
