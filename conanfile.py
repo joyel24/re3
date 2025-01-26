64a422f536aa3a26d105db7896b228606d040302
schm1dtmac <danonmac@icloud.com>
2025-01-26 13:00:49 +0000

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -50,7 +50,7 @@ class Re3Conan(ConanFile):
     def requirements(self):
         self.requires("librw/{}".format(self.version))
         self.requires("mpg123/1.26.4")
-        self.requires("sdl/2.30.9")
+        self.requires("sdl/2.0.16")
         if self.options.audio == "openal":
             self.requires("openal/1.21.0")
         elif self.options.audio == "miles":
