8e408e3ff86b43b638328f7650bd2777c2ad6079
schm1dtmac <danonmac@icloud.com>
2025-01-26 15:11:52 +0000

    Update glfw.cpp

--- src/skel/glfw/glfw.cpp
+++ src/skel/glfw/glfw.cpp
@@ -2357,7 +2357,7 @@ void CapturePad(RwInt32 padID)
 		ControlsManager.m_NewState.mappedButtons[15] = ControlsManager.m_NewState.mappedButtons[16] = 0;
 	}
 
-	ControlsManager.m_NewState.buttons = buttons;
+	ControlsManager.m_NewState.buttons = (uint8*)buttons;
 	ControlsManager.m_NewState.numButtons = numButtons;
 	ControlsManager.m_NewState.id = glfwPad;
 	ControlsManager.m_NewState.isGamepad = true;
