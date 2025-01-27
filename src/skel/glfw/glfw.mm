f005ed6e79997ddef73c2a0b7402cbb0256bfac8
schm1dtmac <danonmac@icloud.com>
2025-01-27 17:02:54 +0000

    Update glfw.mm

--- src/skel/glfw/glfw.mm
+++ src/skel/glfw/glfw.mm
@@ -1183,7 +1183,7 @@ void PlayMovieInWindow(const char* szFile)
     [playerLayer setNeedsDisplay];
     [containerView needsDisplay];
     [view addSubview:containerView];
-    [[avPlayer currentItem] allowedAudioSpatializationFormats:AVAudioSpatializationFormatNone];	
+    [avPlayer.currentItem allowedAudioSpatializationFormats:AVAudioSpatializationFormatNone];	
     [avPlayer play];
     movieplaying = true;
     CFNotificationCenterAddObserver
