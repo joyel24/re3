b4cb9165802a0c5159fc471fa1cdcd37d294fb62
schm1dtmac <danonmac@icloud.com>
2025-01-31 14:32:09 +0000

    Update Hud.cpp

--- src/renderer/Hud.cpp
+++ src/renderer/Hud.cpp
@@ -1217,7 +1217,7 @@ void CHud::Draw()
 				CFont::SetScale(SCREEN_SCALE_X_PC(1.35f), SCREEN_SCALE_Y_PC(2.7f));
 				CFont::SetPropOn();
 				CFont::SetCentreOn();
-				CFont::SetCentreSize(SCALE_AND_CENTER_X(DEFAULT_SCREEN_WIDTH - 128.0f));
+				CFont::SetCentreSize(SCALE_AND_CENTER_X(DEFAULT_SCREEN_WIDTH - 280.0f));
 				CFont::SetColor(CRGBA(255, 255, 0, 255));
 				CFont::SetFontStyle(FONT_HEADING);
 
