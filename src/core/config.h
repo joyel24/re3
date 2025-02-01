ea4580c474aef245275338666203d629a6ae59d3
schm1dtmac <danonmac@icloud.com>
2025-02-01 16:16:10 +0000

    Update config.h

--- src/core/config.h
+++ src/core/config.h
@@ -380,7 +380,7 @@ enum Config {
 #endif
 
 #ifndef USE_ADVANCED_SCRIPT_DEBUG_OUTPUT
-#define USE_BASIC_SCRIPT_DEBUG_OUTPUT
+//#define USE_BASIC_SCRIPT_DEBUG_OUTPUT
 #endif
 
 #ifdef MASTER
@@ -409,7 +409,7 @@ enum Config {
 //#define PEDS_REPORT_CRIMES_ON_PHONE // requires COMPATIBLE_SAVES
 
 // Camera
-#define PS2_CAM_TRANSITION	// old way of transitioning between cam modes
+//#define PS2_CAM_TRANSITION	// old way of transitioning between cam modes
 #define IMPROVED_CAMERA		// Better Debug cam, and maybe more in the future
 #define FREE_CAM		// Rotating cam
 
