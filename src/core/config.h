d639b17af46f149a3daac20acdd82a8956baf321
schm1dtmac <danonmac@icloud.com>
2025-02-01 13:43:01 +0000

    Update config.h

--- src/core/config.h
+++ src/core/config.h
@@ -301,7 +301,7 @@ enum Config {
 #define ANISOTROPIC_FILTERING	// set all textures to max anisotropic filtering
 //#define USE_TEXTURE_POOL
 #ifdef LIBRW
-//#define EXTENDED_COLOURFILTER		// more options for colour filter (replaces mblur)
+#define EXTENDED_COLOURFILTER		// more options for colour filter (replaces mblur)
 #define EXTENDED_PIPELINES		// custom render pipelines (includes Neo)
 #define SCREEN_DROPLETS			// neo water droplets
 //#define NEW_RENDERER		// leeds-like world rendering, needs librw
