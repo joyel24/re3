f5723716f2156ef9a20b7531456f9cec28f6fde0
schm1dtmac <danonmac@icloud.com>
2025-02-08 16:18:10 +0000

    Update re3.cpp

--- src/core/re3.cpp
+++ src/core/re3.cpp
@@ -70,7 +70,7 @@ unsigned long int myrand_seed = 1;
 int
 myrand(void)
 {
-	myrand_seed = std::random_device{}();
+	myrand_seed = (unsigned int)std::random_device{}();
 #ifdef USE_PS2_RAND
 	// Use our own implementation of rand, stolen from PS2
 	return ((myrand_seed >> 32) & 0x7FFFFFFF);
